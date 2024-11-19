1572371da42104c533c0bfbe38091ef64ccff9e5
Your Name <your_email@example.com>
2024-11-19 22:04:27 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 export default defineConfig({
-  base: "/OUTPUT_04-CLAUDE_TSX/",
+  base: process.env.NODE_ENV === 'production' ? '/DASHBOARDS/' : '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
+  server: {
+    historyApiFallback: true
+  }
 });
