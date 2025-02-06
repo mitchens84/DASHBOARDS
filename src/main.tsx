714bcfaee8ffb3911ea0837ca9e623c551a42c19
Your Name <your_email@example.com>
2025-02-06 12:25:35 +0700

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './components/TableOfContents.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/DASHBOARDS">
       <App />
     </BrowserRouter>
   </React.StrictMode>
